3200fc6f3f4a36052477fee9004a08f1543309e0
이선우 <mercyrain23@gmail.com>
2021-01-07 18:52:00 +0900

    first

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+
+    protected Address() { // JPA Spec 상 만든 것. new 로 생성하면 안됨
+    }
+}
