96be3dc1b46c8894f4f62aadd835b7aa0d700f9c
이선우 <mercyrain23@gmail.com>
2021-01-11 21:27:25 +0900

    modify

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,36 @@ public class ItemService {
     public void saveItem(Item item) {
         itemRepository.save(item);
     }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) { // 변경 감지 기능
+        Item findItem = itemRepository.findOne(itemId); // 영속 상태
+//        findItem.change(name, price, stockQuantity); // 이런 메소드를 만들어서 사용하는거 권
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+//        itemRepository.save(findItem); // @Transactional 에 의해서 commit 되고 update 되기 때문에 save 필요없다
+    }
+
+
+//    @Transactional
+//    public void updateItem(Long itemId, Book bookParam) { // 변경 감지기능
+//        Item findItem = itemRepository.findOne(itemId); // 영속 상태
+//        findItem.setPrice(bookParam.getPrice());
+//        findItem.setName(bookParam.getName());
+//        findItem.setStockQuantity(bookParam.getStockQuantity());
+////        itemRepository.save(findItem); // @Transactional 에 의해서 commit 되고 update 되기 때문에 save 필요없다
+//    }
+
+//    @Transactional
+//    public Item updateItem(Long itemId, Book bookParam) { // merge 동작 방식
+//        Item findItem = itemRepository.findOne(itemId); // 영속 상태
+//        findItem.setPrice(bookParam.getPrice());
+//        findItem.setName(bookParam.getName());
+//        findItem.setStockQuantity(bookParam.getStockQuantity());
+//        return findItem;
+//    }
+
 
     public List<Item> findItems() {
         return itemRepository.findAll();
